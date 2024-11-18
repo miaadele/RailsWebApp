8a80f0faabb7d0a1fc1806be7cb5490b63187b65
Mia <123035906+miaadele@users.noreply.github.com>
2024-11-17 17:04:09 -0800

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Product.create!(
+    title: 'Blick Studio Brush Markers',
+    description: %{
+        <p>
+        This 6-piece basic set contains 020 Black, 005 Red, 049 Dark Green, 
+        014 Canary Yellow, 038 Ultramarine, and 034 Violet.
+        </p>
+    },
+    image_url: 'blick.jpg',
+    price: 14.92
+)
